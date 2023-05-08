948f2f8ce6bdf3a95b90d47ce7a5e3a78b33eb01
vtaylor <114200339+Von-Taylor@users.noreply.github.com>
2023-05-08 15:38:55 -0700

    Add files via upload

---
+++ app/src/main/java/sayit/QandA.java
@@ -0,0 +1,19 @@
+package sayit;
+
+public class QandA {
+    private String query;
+    private String answer;
+
+    public QandA(String query, String answer) {
+        this.query = query;
+        this.answer = answer;
+    }
+
+    public String getQuery() {
+        return this.query;
+    }
+
+    public String getAnswer() {
+        return this.answer;
+    }
+}
